dfc50fa3208b95040e0611cb21cee284761d3158
jjuser0110 <163217224+jjuser0110@users.noreply.github.com>
2026-01-29 09:52:59 +0800

    aa

--- header.php
+++ header.php
@@ -1,9 +1,40 @@
 <head>
-    <meta http-equiv="Content-Type" content="text/html; charset=utf-8" />
-    <meta name="viewport" content="width=device-width, initial-scale=1.0, maximum-scale=1.0, user-scalable=no">
-    <meta name="format-detection" content="telephone=no">
-    <meta name="description" content="">
-    <meta name="author" content="">
+    <!-- Basic Meta -->
+    <title>Cott Development Sdn. Bhd. | Construction & Engineering Contractor Malaysia</title>
+    <meta charset="utf-8">
+    <meta http-equiv="X-UA-Compatible" content="IE=edge">
+    <meta name="viewport" content="width=device-width, initial-scale=1">
+
+    <!-- SEO Meta -->
+    <meta name="title" content="Cott Development Sdn. Bhd. | Construction & Engineering Contractor Malaysia">
+    <meta name="description" content="Cott Development Sdn. Bhd. is a trusted Malaysian construction and engineering contractor providing building works, civil engineering, infrastructure projects, and site preparation services with a strong commitment to quality and safety.">
+    <meta name="keywords" content="Cott Development, construction company Malaysia, construction contractor, civil engineering Malaysia, building contractor, infrastructure works, earthworks, site preparation, renovation, general contractor">
+    <meta name="author" content="Cott Development Sdn. Bhd.">
+    <meta name="robots" content="index, follow">
+
+    <!-- Canonical -->
+    <link rel="canonical" href="https://cottdevelopment.com.my/">
+
+    <!-- Open Graph / Facebook -->
+    <meta property="og:type" content="website">
+    <meta property="og:url" content="https://cottdevelopment.com.my/">
+    <meta property="og:title" content="Cott Development Sdn. Bhd. | Construction & Engineering Contractor Malaysia">
+    <meta property="og:description" content="Reliable construction and civil engineering contractor in Malaysia delivering building, infrastructure, and site development projects with quality workmanship and professionalism.">
+    <meta property="og:image" content="https://cottdevelopment.com.my/image/logo.png">
+
+    <!-- Twitter -->
+    <meta name="twitter:card" content="summary_large_image">
+    <meta name="twitter:url" content="https://cottdevelopment.com.my/">
+    <meta name="twitter:title" content="Cott Development Sdn. Bhd. | Construction & Engineering Contractor Malaysia">
+    <meta name="twitter:description" content="Trusted Malaysian contractor for building construction, civil engineering, and infrastructure projects.">
+    <meta name="twitter:image" content="https://cottdevelopment.com.my/image/logo.png">
+
+    <!-- Favicon -->
+    <link rel="icon" href="https://cottdevelopment.com.my/image/logo.png" type="image/x-icon">
+    <link rel="apple-touch-icon" href="https://cottdevelopment.com.my/image/logo.png">
+
+    <!-- Theme Color (optional) -->
+    <meta name="theme-color" content="#1f2933">
 
     <!-- GOOGLE FONT -->
     <link href='https://fonts.googleapis.com/css?family=Montserrat:400,700' rel='stylesheet' type='text/css'>
@@ -40,5 +71,5 @@
         <script src="https://oss.maxcdn.com/libs/respond.js/1.4.2/respond.min.js"></script>
     <![endif]-->
 
-    <title>Cott Development | Homepage</title>
+    <!-- <title>Cott Development | Homepage</title> -->
 </head>
